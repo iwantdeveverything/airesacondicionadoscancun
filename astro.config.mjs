c227d6640a10e8cf98857bdbc9f240c06652ec91
hstrejoluna <trejolunatutoriales@gmail.com>
2026-07-23 08:15:20 -0500

    feat: add sitemap integration for vercel URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
+  site: 'https://airesacondicionadoscancun.vercel.app',
+  integrations: [sitemap()],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
-  site: 'https://airesacondicionadoscancun.com',
 });
